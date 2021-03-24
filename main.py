71ef11c8af2c46e1a528e9bcebb364255f6ec7ae
Replit user <replituser@example.com>
2021-03-24 17:08:47 +0000

    Added auto react

--- main.py
+++ main.py
@@ -9,10 +9,51 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    praise_john = '<:praisejohn:751866320315744330>'
+    crying_ducc = '<:cryingducc:751955222401646694>'
+    
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
         await message.channel.send('Teehee, hello!')
+
+    if message.content.startswith('omegalul'):
+        await message.channel.send('poggers')
+
+    if message.content.startswith('DAILY'):
+        await message.add_reaction('🇩')
+        await message.add_reaction('🇦')
+        await message.add_reaction('🇮')
+        await message.add_reaction('🇱')
+        await message.add_reaction('🇾')
+        await message.add_reaction(praise_john)
+
+    if message.content.startswith('PRAISE'):
+        await message.add_reaction('🇵')
+        await message.add_reaction('🇷')
+        await message.add_reaction('🇦')
+        await message.add_reaction('🇮')
+        await message.add_reaction('🇸')
+        await message.add_reaction('🇪')
+        await message.add_reaction(praise_john)
+
+    if message.content.startswith('JOHNO'):
+        await message.add_reaction('🇯')
+        await message.add_reaction('🇴')
+        await message.add_reaction('🇭')
+        await message.add_reaction('🇳')
+        await message.add_reaction('🅾️')
+        await message.add_reaction(praise_john)
+    elif message.content.startswith('JOHN'):
+        await message.add_reaction('🇯')
+        await message.add_reaction('🇴')
+        await message.add_reaction('🇭')
+        await message.add_reaction('🇳')
+        await message.add_reaction(praise_john)
+
+    if message.content.startswith('sad'):
+        await message.channel.send(crying_ducc)
+
 
 client.run(os.getenv('TOKEN'))
